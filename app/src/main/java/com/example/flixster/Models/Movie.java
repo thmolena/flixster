a75236a77c09ce46d392877c97723cf74c19c107
Molena (Thy) Hoang-Xuan Nguyen <molenathyhoangxuannguyen@users.noreply.github.com>
2020-12-27 13:08:07 -0500

    update

---
+++ app/src/main/java/com/example/flixster/Models/Movie.java
@@ -0,0 +1,60 @@
+package com.example.flixster.Models;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.parceler.Parcel;
+
+import java.util.ArrayList;
+import java.util.List;
+@Parcel
+public class Movie {
+    int movieId;
+    String posterPath;
+    String title;
+    String overview;
+    double rating;
+    // The empty Constructor
+    public Movie(){}
+
+    public Movie(JSONObject jsonObject) throws JSONException{
+        posterPath = jsonObject.getString("poster_path");
+        title = jsonObject.getString("title");
+        overview = jsonObject.getString("overview");
+        rating= jsonObject.getDouble("vote_average");
+        movieId = jsonObject.getInt("id");
+    }
+
+
+
+    public static List<Movie> fromJsonArray(JSONArray movieJsonArray) throws JSONException{
+        List<Movie> movies = new ArrayList<>();
+        for (int i = 0; i < movieJsonArray.length(); i++) {
+
+            movies.add(new Movie(movieJsonArray.getJSONObject(i)));
+
+
+        }
+        return movies;
+
+
+    }
+    public String getPosterPath() {
+        return String.format("https://image.tmdb.org/t/p/w342/%s", posterPath); //w is for the  width. This is hard coded
+    }
+
+    public String getOverview() {
+        return overview;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+    public double getRating(){
+        return rating;
+    }
+
+    public int getMovieId(){
+        return movieId;
+    }
+}
---
+++ app/src/main/java/com/example/flixster/adapters/MovieAdapter.java
@@ -0,0 +1,91 @@
+package com.example.flixster.adapters;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Parcel;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.example.flixster.DetailActivity;
+import com.example.flixster.Models.Movie;
+import  com.example.flixster.R;
+
+import org.parceler.Parcels;
+
+import java.util.List;
+
+public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder>{
+    Context context;
+    List<Movie> movies;
+    public MovieAdapter(Context context, List<Movie> movies){
+        this.context = context;
+        this.movies = movies;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+       View movieView =  LayoutInflater.from(context).inflate(R.layout.item_movie, parent,false);
+        return new ViewHolder(movieView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Log.d("MovieAdapter","onCreateHolder");
+       Movie movie = movies.get(position);
+
+       holder.bind(movie);
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return movies.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        RelativeLayout container;
+        TextView tvTitle;
+        TextView tvOverview;
+        ImageView ivPoster;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            tvTitle = itemView.findViewById(R.id.tvTitle);
+            tvOverview = itemView.findViewById(R.id.tvOverview);
+            ivPoster=itemView.findViewById(R.id.ivPoster);
+            container=itemView.findViewById(R.id.container);
+        }
+
+        public void bind(final Movie movie) {
+            tvTitle.setText(movie.getTitle());
+            tvOverview.setText(movie.getOverview());
+            Glide.with(context).load(movie.getPosterPath()).into(ivPoster);
+            //1. Register click listener on the whole container
+
+            container.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    //2. Navigate to a new activity on the tap
+                    Intent i = new Intent(context, DetailActivity.class);
+                   //i.putExtra("title",movie.getTitle());
+                  i.putExtra("movie", Parcels.wrap(movie));
+                    context.startActivity(i);
+                }
+            });
+
+        }
+    }
+
+}
+
